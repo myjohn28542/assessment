699df7a5323257a46e8a7cd7fd88dd47200aa40d
myjohn28542 <john28542@gmail.com>
2023-01-04 21:54:34 +0700

    getExpenses

---
+++ server.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+
+	"github.com/lib/pq"
+)
+
+type Expenses struct {
+	ID     int      `json:"id"`
+	Title  string   `json:"title"`
+	Amount int      `json:"amount"`
+	Note   string   `json:"note"`
+	Tags   []string `json:"tags"`
+}
+
+type Err struct {
+	Message string `json:"message"`
+}
+
+var db *sql.DB
+
+func getExpenses(c echo.Context) error {
+	stmt, err := db.Prepare("SELECT id, title, amount , note ,tags FROM expenses")
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't prepare query all expenses:" + err.Error()})
+	}
+
+	rows, err := stmt.Query()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, Err{Message: "can't query all expenses:" + err.Error()})
+	}
+
+	expenses := []Expenses{}
+
+	for rows.Next() {
+		exp := Expenses{}
+		err := rows.Scan(&exp.ID, &exp.Title, &exp.Amount, &exp.Note, pq.Array(&exp.Tags))
+		if err != nil {
+			return c.JSON(http.StatusInternalServerError, Err{Message: "can't scan expenses:" + err.Error()})
+		}
+		expenses = append(expenses, exp)
+	}
+
+	return c.JSON(http.StatusOK, expenses)
+}
+
+func main() {
+	var err error
+	url := "postgres://bvmnqtid:TYwIzLz0EPRo-v7Ztb8kYZ-PFjdUCNqE@john.db.elephantsql.com/bvmnqtid"
+	db, err = sql.Open("postgres", url)
+	if err != nil {
+		log.Fatal("Connect to database error", err)
+	}
+	defer db.Close()
+
+	createTb := `
+	CREATE TABLE IF NOT EXISTS expenses ( id SERIAL PRIMARY KEY,
+		title TEXT,
+		amount FLOAT,
+		note TEXT,
+		tags TEXT[] );
+	`
+	_, err = db.Exec(createTb)
+
+	if err != nil {
+		log.Fatal("can't create table", err)
+	}
+
+	e := echo.New()
+
+	e.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+		if username == "expenses" || password == "123456" {
+			return true, nil
+		}
+		return false, nil
+	}))
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/expenses", getExpenses)
+
+	log.Fatal(e.Start(":2565"))
+}
